dda07e8d74f4a30cdbe09e29793814ce6a412c92
lixianhai <lixian_hai@163.com>
2019-12-27 11:00:03 +0800

    npm run build

--- myvue/src/redirect/index.js
+++ myvue/src/redirect/index.js
@@ -5,18 +5,26 @@ import router from '../router'
 import { getToken } from '@/utils/auth'
 import thisVue from '../main'
 
+const whiteList = ['/login', '/auth-redirect'] // no redirect whitelist
+
 router.beforeEach(async(to, form, next)=>{
+    console.log('进入了钩子函数')
 
     const hasToken = getToken()
 
     if(hasToken) {
+        console.log('有token')
         if(to.path === '/login') {
+            console.log('有token to.path=login')
             next()
         }else {
+            console.log('有token to.path！=login')
             const hasRoles = store.state.user.roles && store.state.user.roles.length > 0
             if (hasRoles) {
+                console.log('有路由列表')
                 next()
             }else {
+                console.log('添加路由列表')
                 try {
                     const { roles } = await store.dispatch('getUserInfo')
                     const accessRoutes = await store.dispatch('generateRoutes', roles)
@@ -32,7 +40,13 @@ router.beforeEach(async(to, form, next)=>{
             next()
         }
     }else {
-        next();
+        if (whiteList.indexOf(to.path) !== -1) {
+            // in the free login whitelist, go directly
+            next()
+        } else {
+        // other pages that do not have permission to access are redirected to the login page.
+            next('/login')
+        }
     }
     if(thisVue) {
         document.title = thisVue.$t(filterLanguages(thisVue.$t('navBar'),'navBar',to.meta.languages)) + ' - Vue Element Admin'
